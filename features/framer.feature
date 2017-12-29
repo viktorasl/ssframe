3af4ca9c8f41d89eb597369d02cc73298c7c7794
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2017-12-29 16:05:35 +0200

    Passes promo texts config as json text param

--- features/framer.feature
+++ features/framer.feature
@@ -2,6 +2,6 @@ Feature: Framer
 
   Scenario: Framer produces correct output
     Given I use a fixture named "frame"
-    When I run `ssframe frame`
+    When I run `ssframe frame '{"en-GB":[{"2-calendar":"Stay up to date with your\'n'whole salon'"'"'s calendar"}],"de-DE":[{"2-calendar":"Mit dem gesamten Kalender\'n'behalten Sie immer den Überblick"}]}'`
     Then a file named "ssframe/en-GB/iPhone X-2-calendar.png" should exist
     And a file named "ssframe/de-DE/iPhone X-2-calendar.png" should exist
--- lib/ssframe/cli.rb
+++ lib/ssframe/cli.rb
@@ -1,20 +1,14 @@
 require "thor"
 require "ssframe"
+require "json"
 
 module Ssframe
   class CLI < Thor
 
-    desc "frame", "Frames screenshots into frames with promo text"
-    def frame()
-      titles_config = {
-        "en-GB" => [
-          {"2-calendar" => "Stay up to date with your\nwhole salon's calendar"}
-        ],
-        "de-DE" => [
-          {"2-calendar" => "Mit dem gesamten Kalender\nbehalten Sie immer den Überblick"}
-        ]
-      }
-      Ssframe::Framer.frame(titles_config, ['iPhone X'], "#69c748", "font.otf")
+    desc "frame TEXTS", "Frames screenshots into frames with promo texts"
+    def frame(texts_json)
+      titles_config = JSON.parse(texts_json)
+      Ssframe::Framer.frame(titles_config, ['iPhone X'], "#b50039", "font.otf")
     end
   end
 end
